c435bbb3e023704ed7d14a93ba2f9f7188621f96
Terminatedzz <131369904+Terminatedzz@users.noreply.github.com>
2024-04-25 18:20:01 +0200

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,3 @@
+-----------------------------------------------
+if u need help with setup or something else join the support server below ⬇️
+https://discord.gg/Dt5pN3B2Xj
---
+++ setup.php
@@ -0,0 +1,7 @@
+<?php
+$color = 'Your color'; // any hexdec color
+$image = 'Your Image'; // get any image (must be url)
+$name = 'Your Name'; // name for your site
+$discord = 'https://discord.gg/Yourinvite'; // discord invite, must be starting with https://
+$webhook = 'your webhook'; // discord webhook, make sure you put in the right webhook
+?>

6dd8df3c89c594d486fdbf2a95c258cc8182b5c6
Terminatedzz <131369904+Terminatedzz@users.noreply.github.com>
2024-04-27 20:30:52 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -3,5 +3,5 @@ $color = 'Your color'; // any hexdec color
 $image = 'Your Image'; // get any image (must be url)
 $name = 'Your Name'; // name for your site
 $discord = 'https://discord.gg/Yourinvite'; // discord invite, must be starting with https://
-$webhook = 'your webhook'; // discord webhook, make sure you put in the right webhook
+$admin = 'your webhook'; // discord webhook, make sure you put in the right webhook
 ?>
